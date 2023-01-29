f3b2794b9028e3b307728e868ae0c44f610ef154
Ubuntu <ubuntu@ip-172-31-13-205.us-west-1.compute.internal>
2023-01-29 22:22:24 +0000

    add example config files

---
+++ RDS/config_example.py
@@ -0,0 +1,5 @@
+rds = {'username':'Your RDS credentials',
+        'password':'Your RDS credentials',
+        'database_name':'Your RDS credentials',
+        'port':'Your RDS credentials',
+        'endpoint':'Your RDS credentials'}
---
+++ config_example.py
@@ -0,0 +1,15 @@
+sheet_links = {
+    'heart_rate':'Your Google Sheet ID',
+    'steps':'Your Google Sheet ID',
+    'active_energy':'Your Google Sheet ID',
+    'resting_energy':'Your Google Sheet ID',
+    'distance':'Your Google Sheet ID'}
+
+webids = {
+    'heart_rate':'Your App Script Deployment ID',
+    'steps':'Your App Script Deployment ID',
+    'active_energy':'Your App Script Deployment ID',
+    'resting_energy':'Your App Script Deployment ID',
+    'distance':'Your App Script Deployment ID'}
+
+aws_bucket_name = 'Your S3 Bucket Name'
